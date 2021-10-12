5fd7c3a587f1e512011e93ac950c34a10afe8e88
Markd <markd@rhel8wk1.xphyrlab.net>
2021-10-12 18:48:21 -0400

    working build

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:0f046d61ecc9855e0b7d1846d645661b32654a358c1530dd20efb31f65686f8d
+#FROM quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:0f046d61ecc9855e0b7d1846d645661b32654a358c1530dd20efb31f65686f8d
+FROM quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:bb5c296b4a6125f475906f457cdf2ffd35d3d70d333073a7735c2dfb29225d6d
 
 RUN dnf -y install git make sudo gcc \
 && dnf clean all \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 /usr/sbin/rmmod nvidia
-/root/nvidia/NVIDIA-Linux-x86_64-470.63-vgpu-kvm.run --kernel-source-path=/usr/src/kernels/4.18.0-305.17.1.el8_4.x86_64 --kernel-install-path=/lib/modules/4.18.0-305.17.1.el8_4.x86_64/kernel/drivers/video/ --silent --tmpdir /root/tmp/ --no-systemd
+/root/nvidia/NVIDIA-Linux-x86_64-470.63-vgpu-kvm.run --kernel-source-path=/usr/src/kernels/4.18.0-305.19.1.el8_4.x86_64 --kernel-install-path=/lib/modules/4.18.0-305.19.1.el8_4.x86_64/kernel/drivers/video/ --silent --tmpdir /root/tmp/ --no-systemd
 
 /usr/bin/nvidia-vgpud &
 /usr/bin/nvidia-vgpu-mgr &
 
-while true; do sleep 15 ; /usr/bin/pgrep nvidia-vgpu-mgr $? -eq 0 || echo "nvidia-vgpu-mgr is not running" && exit 1; done
+while true; 
+do 
+  sleep 15
+  /usr/bin/pgrep nvidia-vgpu-mgr 
+  if [ $? -ne 0 ]
+  then
+    echo "nvidia-vgpu-mgr is not running" && exit 1
+  fi
+done
 
